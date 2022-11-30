b5010fc71ff8aad3d3e06a71f1aa033c9b2f672d
pietromartincigh <pietro.martincigh@isitgo.it>
2022-11-30 09:18:40 +0100

    variabili main

--- martincigh_scordo_verifica/Main.java
+++ martincigh_scordo_verifica/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
     public static void main (String args[]){
 
+        Aereo aerei[] = new Aereo[3];
 
-        
+        aerei[0]= new Aereo("0Q2E45S", 124, "Toyota", "Boeing 737");
+        aerei[1]= new Aereo("XCTY674O", 31, "Peugeot", "Schifo con le ali");
+        aerei[2]= new Aereo("EW8213HBL0", 5783, "Subaru", "Stuka");
+
+        Aereoporto aereoporto = new Aereoporto("Mogadiscio", 57, aerei);
+
     }
     
 }
