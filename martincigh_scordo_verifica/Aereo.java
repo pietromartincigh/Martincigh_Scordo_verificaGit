982125074e48333a21f58860224787ae7d37b09b
scordochristopher <christopher.scordo@isitgo.it>
2022-11-30 08:51:05 +0100

    creazione classe aereo

---
+++ martincigh_scordo_verifica/Aereo.java
@@ -0,0 +1,51 @@
+package martincigh_scordo_verifica;
+
+public class Aereo{
+
+String codice;
+int nPosti;
+String marca;
+String modello;
+
+public Aereo(){}
+
+public Aereo(String codice, int nPosti, String marca, String modello){
+    this.codice=codice;
+    this.nPosti=nPosti;
+    this.marca=marca;
+    this.modello=modello;
+}
+
+public void setCodice(String codice){
+    this.codice=codice;
+}
+
+public void setNPosti(int nPosti){
+    this.nPosti=nPosti;
+}
+
+public void setMarca(String marca){
+    this.marca=marca;
+}
+
+public void setModello(String modello){
+    this.modello=modello;
+}
+
+public String getCodice(){
+    return codice;
+}
+
+public int getNposti(){
+    return nPosti;
+}
+
+public String getMarca(){
+    return marca;
+}
+
+public String getModello(){
+    return modello;
+}
+
+}
