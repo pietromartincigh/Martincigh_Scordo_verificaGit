45d831421bce0fb0eff3ecda5d15899446136242
scordochristopher <christopher.scordo@isitgo.it>
2022-11-30 09:34:13 +0100

    correzioni to string

--- martincigh_scordo_verifica/Main.java
+++ martincigh_scordo_verifica/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main (String args[]){
 
+        System.out.printl("L'aeroporto di città " + aeroporto.getCitta() + "ha una superficie di " + aeroporto.getSuperficie() + "metri quadrati.");
+
+        for(int=0; i<3; i++){
+        System.out.println("Gli aerei disponibili sono: " + aerei[i].toString());
+        }
 
         
     }
